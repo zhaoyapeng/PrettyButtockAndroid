5cb36daa44f19b90a80b21b12e50ff500285f74d
du <du@du-PC>
2015-05-08 21:27:48 +0800

    播放视频

--- src/com/lary/health/ui/PlayVideoActivity.java
+++ src/com/lary/health/ui/PlayVideoActivity.java
@@ -1,30 +1,58 @@
 package com.lary.health.ui;
 
+import java.text.MessageFormat;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
 import com.lary.health.R;
 
 import android.content.Context;
 import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.media.AudioManager.OnAudioFocusChangeListener;
+import android.media.MediaPlayer.OnErrorListener;
+import android.media.MediaPlayer.OnPreparedListener;
 import android.net.Uri;
+import android.os.Handler;
+import android.view.GestureDetector;
+import android.view.GestureDetector.OnGestureListener;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.SeekBar;
+import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.VideoView;
+import android.widget.SeekBar.OnSeekBarChangeListener;
 
-public class PlayVideoActivity extends BaseFragmentActivity implements OnClickListener{
+public class PlayVideoActivity extends BaseFragmentActivity implements OnClickListener,OnGestureListener{
 	
 	private String url="http://114.113.237.28/family/./Public/Uploads/20140812/53e9bbc65e1d9.mp4";
 	private VideoView videoView;
 	private ImageView startBt;
 	private Uri mUri;
+	private ScheduledExecutorService scheduledExecutorService;
+	private View mediaControllerLayout;
+	private SeekBar videoSeekBar;
+	private GestureDetector gestureDetector;
+	private ProgressBar pbLoad;
+	private TextView currentTimeView;
+	private TextView totalTimeView;
+	private Handler handler;
+	private int mPositionWhenPaused = -1;
 
 	//private 
 	@Override
 	protected void initData() {
 		// TODO Auto-generated method stub
 		mUri = Uri.parse(url);
+		this.scheduledExecutorService = Executors.newScheduledThreadPool(2);
 
+		this.gestureDetector = new GestureDetector(PlayVideoActivity.this);
+		this.handler = new Handler();
 	}
 
 	@Override
@@ -33,20 +61,120 @@ public class PlayVideoActivity extends BaseFragmentActivity implements OnClickLi
 		setContentView(R.layout.video_view);
 		videoView = (VideoView) findViewById(R.id.my_video_view);
 		startBt = (ImageView) findViewById(R.id.playButton);
+		mediaControllerLayout = findViewById(R.id.mediaControllerLayout);
+		pbLoad = (ProgressBar) findViewById(R.id.pb_load);
+		currentTimeView = (TextView) findViewById(R.id.currentTimeView);
+		totalTimeView = (TextView) findViewById(R.id.totalTimeView);
+		videoSeekBar = (SeekBar) findViewById(R.id.videoSeekBar);
+		videoSeekBar.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
+			
+			@Override
+			public void onStopTrackingTouch(SeekBar seekBar) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void onStartTrackingTouch(SeekBar seekBar) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void onProgressChanged(SeekBar seekBar, int progress,
+					boolean fromUser) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+		startBt.setImageResource(R.drawable.mv_pause_button);
+		startBt.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				if (videoView.isPlaying()) {
+					videoView.pause();
+					startBt.setImageResource(R.drawable.mv_start_button);
+				} else {
+					videoView.start();
+					startBt.setImageResource(R.drawable.mv_pause_button);
+				}
+			}
+		});
+		
+		videoView.setOnErrorListener(new OnErrorListener() {
+			
+			@Override
+			public boolean onError(MediaPlayer mp, int what, int extra) {
+				// TODO Auto-generated method stub
+				pbLoad.setVisibility(View.GONE);
+				return false;
+			}
+		});
+		videoView.setOnPreparedListener(new OnPreparedListener() {
+			
+			@Override
+			public void onPrepared(MediaPlayer mp) {
+				// TODO Auto-generated method stub
+				pbLoad.setVisibility(View.GONE);
+				mp.start();
+				startBt.setImageResource(R.drawable.mv_pause_button);
+				hideControllView();
+				scheduledExecutorService.scheduleAtFixedRate(new Runnable() {
+
+					@Override
+					public void run() {
+						handler.post(new Runnable() {
+							@Override
+							public void run() {
+								if (videoView.isPlaying()) {
+									float position = videoView
+											.getCurrentPosition();
+									int bufferPos = videoView.getBufferPercentage();
+									int duration = videoView.getDuration();
+									videoSeekBar.setSecondaryProgress(bufferPos);
+									totalTimeView.setText(getTimeFormatValue(duration));
+									currentTimeView
+											.setText(getTimeFormatValue((int) position));
+									videoSeekBar.setProgress((int) (position
+											/ duration * videoSeekBar.getMax()));
+								}
+							}
+						});
+
+					}
+				}, 1000, 1000, TimeUnit.MILLISECONDS);
+			}
+		});
+	}
+
+	private static String getTimeFormatValue(long time) {
+		return MessageFormat.format(
+				"{0,number,00}:{1,number,00}:{2,number,00}",
+				time / 1000 / 60 / 60, time / 1000 / 60 % 60, time / 1000 % 60);
+	}
+	
+	private void hideControllView(){
+		handler.postDelayed(new Runnable() {
+			@Override
+			public void run() {
+				mediaControllerLayout.setVisibility(View.INVISIBLE);
+			}
+		}, 5000);
 	}
 
 	@Override
 	protected void initWidgetAciotns() {
 		// TODO Auto-generated method stub
-		startBt.setOnClickListener(this);
 		focusForMidea();
 		if (videoView != null && mUri != null) {
-			//pbLoad.setVisibility(View.VISIBLE);
+			pbLoad.setVisibility(View.VISIBLE);
 			videoView.setVideoURI(mUri);
 			videoView.requestFocus();
 			videoView.start();
 		} else {
-			Toast.makeText(this, "无法播放此视频", Toast.LENGTH_SHORT)
+			Toast.makeText(this, "视频无法播放", Toast.LENGTH_SHORT)
 					.show();
 		}
 	}
@@ -73,29 +201,29 @@ public class PlayVideoActivity extends BaseFragmentActivity implements OnClickLi
                     public void onAudioFocusChange(int focusChange) {
                         switch (focusChange) {
                         case AudioManager.AUDIOFOCUS_GAIN:
-                            // 获得音频焦点
+                            // 鑾峰緱闊抽鐒︾偣
                             if (!videoView.isPlaying()) {
                             	videoView.start();
                             //	playButton.setImageResource(R.drawable.mv_pause_button);
                             }
-                            // 还原音量
+                            // 杩樺師闊抽噺
 //                            mediaPlayer.setVolume(1.0f, 1.0f);
                             break;
 
                         case AudioManager.AUDIOFOCUS_LOSS:
-                            // 长久的失去音频焦点，释放MediaPlayer
+                            // 闀夸箙鐨勫け鍘婚煶棰戠劍鐐癸紝閲婃斁MediaPlayer
                             if (videoView.isPlaying())
                             	videoView.stopPlayback();
                             break;
 
                         case AudioManager.AUDIOFOCUS_LOSS_TRANSIENT:
-                            // 展示失去音频焦点，暂停播放等待重新获得音频焦点
+                            // 灞曠ず澶卞幓闊抽鐒︾偣锛屾殏鍋滄挱鏀剧瓑寰呴噸鏂拌幏寰楅煶棰戠劍鐐�
                             if (videoView.isPlaying())
                             	videoView.pause();
                         //    playButton.setImageResource(R.drawable.mv_start_button);
                             break;
                         case AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK:
-                            // 失去音频焦点，无需停止播放，降低声音即可
+                            // 澶卞幓闊抽鐒︾偣锛屾棤闇�鍋滄鎾斁锛岄檷浣庡０闊冲嵆鍙�
                             if (videoView.isPlaying()) {
 //                            	videoView.setVolume(0.1f, 0.1f);
                             }
@@ -105,4 +233,93 @@ public class PlayVideoActivity extends BaseFragmentActivity implements OnClickLi
                 }, AudioManager.STREAM_MUSIC,
                 AudioManager.AUDIOFOCUS_GAIN);
 	}
+
+	@Override
+	public boolean onDown(MotionEvent e) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+
+	@Override
+	public void onShowPress(MotionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public boolean onSingleTapUp(MotionEvent e) {
+		// TODO Auto-generated method stub
+		if (videoView.isPlaying()) {
+			videoView.pause();
+			startBt.setImageResource(R.drawable.mv_start_button);
+		} else {
+			videoView.start();
+			startBt.setImageResource(R.drawable.mv_pause_button);
+		}
+		return false;
+	}
+
+
+	@Override
+	public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX,
+			float distanceY) {
+		// TODO Auto-generated method stub
+		if (distanceX > 0) {
+			this.videoSeekBar
+					.setProgress(this.videoSeekBar.getProgress() - 1);
+		} else {
+			this.videoSeekBar
+					.setProgress(this.videoSeekBar.getProgress() + 1);
+		}
+		videoView.seekTo((int) (this.videoSeekBar.getProgress() * 1.0
+				/ videoSeekBar.getMax() * videoView.getDuration()));
+		return false;
+	}
+
+
+	@Override
+	public void onLongPress(MotionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX,
+			float velocityY) {
+		// TODO Auto-generated method stub
+		
+		return false;
+	}
+
+	@Override
+	public boolean onTouchEvent(MotionEvent event) {
+		// TODO Auto-generated method stub
+		if(event.getAction() == MotionEvent.ACTION_DOWN){
+			mediaControllerLayout.setVisibility(View.VISIBLE);
+		}
+		if(event.getAction() == MotionEvent.ACTION_UP){
+			hideControllView();
+		}
+		return this.gestureDetector.onTouchEvent(event);
+	}
+	
+	@Override
+	protected void onResume() {
+		// TODO Auto-generated method stub
+		super.onResume();
+		if (mPositionWhenPaused >= 0) {
+			videoView.seekTo(mPositionWhenPaused);
+			mPositionWhenPaused = -1;
+		}
+	}
+	@Override
+	protected void onStart() {
+		// TODO Auto-generated method stub
+		super.onStart();
+		initWidgetAciotns();
+	}
+	
 }
--- src/com/lary/health/ui/fragment/HomeFragment.java
+++ src/com/lary/health/ui/fragment/HomeFragment.java
@@ -7,6 +7,7 @@ import netlib.model.HomeDirect;
 
 import com.lary.health.R;
 import com.lary.health.ui.GymnasticsBeautifulActivity;
+import com.lary.health.ui.PlayVideoActivity;
 import com.lary.health.ui.adaper.HomeFunctionAdapter;
 import com.lary.health.ui.widget.RollViewPager2;
 import com.nostra13.universalimageloader.core.DisplayImageOptions;
@@ -40,22 +41,27 @@ public class HomeFragment extends BaseViewPagerFragment {
 	private ArrayList<String> uriList;
 	private ArrayList<View> dots;
 	private GridView func_gridview;
-	List<HomeDirect> homedirects = null;//6大功能
+	List<HomeDirect> homedirects = null;// 6大功能
 	private HomeFunctionAdapter gridAdapter = null;
 
 	@Override
 	protected void initData() {
 		imageLoader = ImageLoader.getInstance();
-		avatarOptions = new DisplayImageOptions.Builder().showImageOnLoading(R.drawable.ic_launcher)
-				.showImageForEmptyUri(R.drawable.ic_launcher).showImageOnFail(R.drawable.ic_launcher)
-				.cacheInMemory(true).cacheOnDisk(true).considerExifParams(true).displayer(new SimpleBitmapDisplayer())
+		avatarOptions = new DisplayImageOptions.Builder()
+				.showImageOnLoading(R.drawable.ic_launcher)
+				.showImageForEmptyUri(R.drawable.ic_launcher)
+				.showImageOnFail(R.drawable.ic_launcher).cacheInMemory(true)
+				.cacheOnDisk(true).considerExifParams(true)
+				.displayer(new SimpleBitmapDisplayer())
 				.imageScaleType(ImageScaleType.EXACTLY).build();
 	}
 
 	@Override
-	protected View initViews(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+	protected View initViews(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
 		View view = inflater.inflate(R.layout.fragment_home, container, false);
-		rollViewPager2 = (RollViewPager2) view.findViewById(R.id.forimg_viewpager);
+		rollViewPager2 = (RollViewPager2) view
+				.findViewById(R.id.forimg_viewpager);
 		func_gridview = (GridView) view.findViewById(R.id.func_gridview);
 		initItems();
 		gridAdapter = new HomeFunctionAdapter(getActivity(), homedirects);
@@ -71,18 +77,29 @@ public class HomeFragment extends BaseViewPagerFragment {
 
 	@Override
 	protected void initWidgetActions() {
-		String url ="http://a.hiphotos.baidu.com/image/w%3D230/sign=1a3ed992a60f4bfb8cd09957334f788f/1e30e924b899a901d434ed3b1f950a7b0208f5be.jpg";
-	//	imageLoader.displayImage(url, ceshiImage, avatarOptions);
+		String url = "http://a.hiphotos.baidu.com/image/w%3D230/sign=1a3ed992a60f4bfb8cd09957334f788f/1e30e924b899a901d434ed3b1f950a7b0208f5be.jpg";
+		// imageLoader.displayImage(url, ceshiImage, avatarOptions);
 		forImg();
-		
+
 		func_gridview.setOnItemClickListener(new OnItemClickListener() {
 
 			@Override
-			public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                  if(position==0){
-                	  Intent intent = new Intent(mContext,GymnasticsBeautifulActivity.class);
-                	  startActivity(intent);
-                  }				
+			public void onItemClick(AdapterView<?> parent, View view,
+					int position, long id) {
+				Intent intent = new Intent(mContext,
+						GymnasticsBeautifulActivity.class);
+				switch (position) {
+				case 0:
+					intent.setClass(getActivity(),GymnasticsBeautifulActivity.class);
+					startActivity(intent);
+					break;
+				case 1:
+					intent.setClass(getActivity(),PlayVideoActivity.class);
+					startActivity(intent);
+					break;
+				default:
+					break;
+				}
 			}
 		});
 	}
@@ -91,24 +108,23 @@ public class HomeFragment extends BaseViewPagerFragment {
 	protected void refreshData() {
 
 	}
-	
-	private void initItems()
-	{
-		String name[] ={"垫上美操","体质辨识","音乐养生","原创音乐","最新活动","锻炼记录"};
-		int [] image = {R.drawable.list01,R.drawable.list02,R.drawable.list03,
-				R.drawable.list04,R.drawable.list05,R.drawable.list06};
-		 homedirects =new  ArrayList<HomeDirect>();
-		 for(int i=1;i<image.length +1 ;i++)
-		 {
-			 HomeDirect direct = new HomeDirect();
-			 direct.setId(i);
-			 direct.setName(name[i-1]);
-			 direct.setImage(image[i-1]);
-			 homedirects.add(direct);
-		 }
+
+	private void initItems() {
+		String name[] = { "垫上美操", "体质辨识", "音乐养生", "原创音乐", "最新活动", "锻炼记录" };
+		int[] image = { R.drawable.list01, R.drawable.list02,
+				R.drawable.list03, R.drawable.list04, R.drawable.list05,
+				R.drawable.list06 };
+		homedirects = new ArrayList<HomeDirect>();
+		for (int i = 1; i < image.length + 1; i++) {
+			HomeDirect direct = new HomeDirect();
+			direct.setId(i);
+			direct.setName(name[i - 1]);
+			direct.setImage(image[i - 1]);
+			homedirects.add(direct);
+		}
 	}
-	
-	public void forImg(){
+
+	public void forImg() {
 		// 构造网络图片
 		uriList = new ArrayList<String>();
 		uriList.add("http://h.hiphotos.baidu.com/album/w%3D2048/sign=730e7fdf95eef01f4d141fc5d4c69825/94cad1c8a786c917b8bf9482c83d70cf3ac757c9.jpg");
@@ -116,24 +132,25 @@ public class HomeFragment extends BaseViewPagerFragment {
 		uriList.add("http://c.hiphotos.baidu.com/album/w%3D2048/sign=a8631adb342ac65c67056173cfcab011/b8389b504fc2d56217d11656e61190ef77c66cb4.jpg");
 		uriList.add("http://e.hiphotos.baidu.com/album/w%3D2048/sign=ffac8994a71ea8d38a227304a332314e/1ad5ad6eddc451da4d9d32c4b7fd5266d01632b1.jpg");
 		uriList.add("http://a.hiphotos.baidu.com/album/w%3D2048/sign=afbe93839a504fc2a25fb705d1e5e611/d058ccbf6c81800a99489685b03533fa838b478f.jpg");
-		rollViewPager2.setDot(dots, R.drawable.dot_focus, R.drawable.dot_normal);
+		rollViewPager2
+				.setDot(dots, R.drawable.dot_focus, R.drawable.dot_normal);
 
-		
 		// 用来显示的点
 
-		rollViewPager2.setPagerCallback(new RollViewPager2.OnPagerClickCallback() {
-			
-			@Override
-			public void onPagerClick(int position) {
-				// TODO Auto-generated method stub
-				Toast.makeText(getActivity(),
-						"第" + position + "张图片被点击了", 0).show();
-			}
-		});
-		
-		//mViewPager.setResImageIds(imageIDs);//设置res的图片id
-		rollViewPager2.setUriList(uriList);//设置网络图片的url
-		rollViewPager2.startRoll();//不调用的话不滚动
+		rollViewPager2
+				.setPagerCallback(new RollViewPager2.OnPagerClickCallback() {
+
+					@Override
+					public void onPagerClick(int position) {
+						// TODO Auto-generated method stub
+						Toast.makeText(getActivity(),
+								"第" + position + "张图片被点击了", 0).show();
+					}
+				});
+
+		// mViewPager.setResImageIds(imageIDs);//设置res的图片id
+		rollViewPager2.setUriList(uriList);// 设置网络图片的url
+		rollViewPager2.startRoll();// 不调用的话不滚动
 	}
 
 }
