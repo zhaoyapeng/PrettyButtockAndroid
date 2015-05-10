d28a887da991c6b15611fc38bfeea7751e55ad70
ruanjianyujian <260913029@qq.com>
2015-05-10 14:25:36 +0800

    对的

---
+++ src/com/lary/health/service/model/UserInfoModel.java
@@ -0,0 +1,55 @@
+package com.lary.health.service.model;
+
+public class UserInfoModel {
+
+	private String id;
+	private String nickname;
+	private String email;
+	private String mobile;
+	private String address;
+	private String qq;
+	private String imageUrl;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getNickname() {
+		return nickname;
+	}
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getMobile() {
+		return mobile;
+	}
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getQq() {
+		return qq;
+	}
+	public void setQq(String qq) {
+		this.qq = qq;
+	}
+	public String getImageUrl() {
+		return imageUrl;
+	}
+	public void setImageUrl(String imageUrl) {
+		this.imageUrl = imageUrl;
+	}
+
+}
